2cbdb03706686f249697f65ccc5ee7c4c046ffb2
payal105 <payaladhikary2000@gmail.com>
2022-07-04 13:21:13 +0530

    first commit

---
+++ data.js
@@ -0,0 +1,40 @@
+let movies = [
+  {
+    name: "loki",
+    des:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Pariatur minus nobis voluptate neque, nesciunt sint?",
+    image: "images/slider 1.PNG"
+  },
+    {
+      name: "falcon and the winter soldier",
+      des:
+        "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Pariatur minus nobis voluptate neque, nesciunt sint?",
+  
+      image: "images/slider 2.PNG"
+    },
+  
+    
+  
+    {
+      name: "wanda vision",
+      des:
+        "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Pariatur minus nobis voluptate neque, nesciunt sint?",
+      image: "images/slider 3.PNG"
+    },
+  
+    {
+      name: "raya and the last dragon",
+      des:
+        "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Pariatur minus nobis voluptate neque, nesciunt sint?",
+      image: "images/slider 4.PNG"
+    },
+  
+    {
+      name: "luca",
+      des:
+        "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Pariatur minus nobis voluptate neque, nesciunt sint?",
+      image: "images/slider 5.PNG"
+    }
+  ];
+  
+  
